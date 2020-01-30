ddfaa2db0a5a2a86fc7a92d570f4d8d1822567e6
oleksandr.sotnikov <oleksandr.sotnikov@vakoms.com.ua>
2020-01-30 11:20:40 +0200

    defensive delete brand

--- src/main/java/com/example/demospring/bike/JpaBike.java
+++ src/main/java/com/example/demospring/bike/JpaBike.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface JpaBike extends JpaRepository<Bike, Long> {
 
+    boolean existsByBrandId(Long id);
 }
--- src/main/java/com/example/demospring/brand/BrandController.java
+++ src/main/java/com/example/demospring/brand/BrandController.java
@@ -1,7 +1,10 @@
 package com.example.demospring.brand;
 
 import com.example.demospring.Exception.DoesNotExistException;
+import com.example.demospring.Exception.ImpossibleToDeleteException;
 import com.example.demospring.Exception.NotFoundException;
+import com.example.demospring.bike.JpaBike;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,10 +14,11 @@ import java.util.List;
 public class BrandController {
 
     private final JpaBrand jpaBrand;
+    private final JpaBike jpaBike;
 
-
-    public BrandController(JpaBrand jpaBrand) {
+    public BrandController(JpaBrand jpaBrand, JpaBike jpaBike) {
         this.jpaBrand = jpaBrand;
+        this.jpaBike = jpaBike;
     }
 
     @GetMapping("/brands")
@@ -44,8 +48,11 @@ public class BrandController {
     public ResponseEntity<Object> delete(@RequestBody Brand brand,
                                          @PathVariable Long id) {
         jpaBrand.findById(id).orElseThrow(() -> new DoesNotExistException("Bad Request", "Incorrect brand's ID'"));
-        jpaBrand.deleteById(id);
+
+        if (jpaBike.existsByBrandId(id)) {
+            throw new ImpossibleToDeleteException("Impossible to delete", "Some bikes belong to this brand");
+        }
+
         return ResponseEntity.noContent().build();
     }
-
 }
