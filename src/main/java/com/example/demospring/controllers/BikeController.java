b657f6b90f58df56c268eb28f9102651afff28ed
oleksandr.sotnikov <oleksandr.sotnikov@vakoms.com.ua>
2020-01-31 14:57:51 +0200

    put for bike

--- src/main/java/com/example/demospring/controllers/BikeController.java
+++ src/main/java/com/example/demospring/controllers/BikeController.java
@@ -50,4 +50,15 @@ public class BikeController {
 
         return jpaBike.save(bike);
     }
+
+
+    //todo Name to uppercase, edit brand name in bike
+    @PutMapping("/bikes/{id}")
+    public Bike edit(@RequestBody Bike bike,
+                     @PathVariable Long id){
+        jpaBike.findById(id).orElseThrow(() -> new DoesNotExistException("Bad Request", "Incorrect bike's ID"));
+        bike.setId(id);
+        return jpaBike.save(bike);
+    }
+
 }
